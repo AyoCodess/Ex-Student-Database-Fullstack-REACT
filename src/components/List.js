86b40e0c566b5e795135602f779e1b466b5ae96b
AyoCodess <ayo.daniel.adesanya@gmail.com>
2022-07-17 20:25:04 +0200

    reset database to reset table btn update

--- src/components/List.js
+++ src/components/List.js
@@ -90,7 +90,7 @@ export const List = () => {
                   setSearchInput('');
                 }}
                 className='inline-flex items-center justify-center rounded-md border border-transparent bg-blue-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 sm:w-auto'>
-                Reset Database
+                Reset Table
               </button>
             </div>
           </div>
